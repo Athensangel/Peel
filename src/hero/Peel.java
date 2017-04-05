f69e925df788980b168794cb5656a977cad40177
1449812719@qq.com <1449812719@qq.com>
2017-04-05 22:17:46 +0800

    亲，吃香蕉嘛

---
+++ src/hero/Peel.java
@@ -0,0 +1,13 @@
+package hero;
+
+public class Peel {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		peel();
+	}
+
+	public static void peel() {
+		System.out.println("香蕉踩了香蕉皮，滑倒了！");
+	}
+}
