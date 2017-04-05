9d847af2da00aed0476779d329b8450d27ec439b
1449812719@qq.com <1449812719@qq.com>
2017-04-05 22:22:00 +0800

    出去

--- src/hero/Peel.java
+++ src/hero/Peel.java
@@ -5,9 +5,13 @@ public class Peel {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		peel();
+		xx();
 	}
 
 	public static void peel() {
 		System.out.println("香蕉踩了香蕉皮，滑倒了！");
+	}
+	public static void  xx(){
+		System.out.println("get out!");
 	}
 }
